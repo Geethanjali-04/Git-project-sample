de65d4d25f98e4a6690fff89d39bc15254ad4274
Geethanjali-04 <geethag2021@gmail.com>
2024-09-23 11:29:53 +0530

    hoisting and variable environment

--- js_sample.js
+++ js_sample.js
@@ -1,2 +1,21 @@
 console.log("hai");
-console.log("while");
+var a = 100
+ // creates a separate execution context and push into callstack (so this variable inside function has memory) once executed it gets removed
+function ai(){
+    var a = 10;
+    console.log(a);
+ }
+
+
+ // creates a separate execution context and push into callstack (so this variable inside function has memory) once executed it gets removed
+  function b(){
+    var a = 20;
+    console.log(a);
+ }
+ ai();
+ b();
+
+ console.log(a);
+
+ //output 10 20 100
+
