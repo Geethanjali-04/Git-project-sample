bff091ce7ed7fe30ca1e55cf2a4d63466019f86b
Geethanjali-04 <geethag2021@gmail.com>
2024-09-23 20:15:44 +0530

    lexical scoping of variables

--- js_sample.js
+++ js_sample.js
@@ -1,2 +1,34 @@
 console.log("hai");
 console.log("while");
+
+var a = 100
+ // creates a separate execution context and push into callstack (so this variable inside function has memory) once executed it gets removed
+function ai(){
+    var a = 10;
+    console.log(a);
+ }
+ // creates a separate execution context and push into callstack (so this variable inside function has memory) once executed it gets removed
+  function b(){
+    var a = 20;
+    console.log(a);
+ }
+ ai();
+ b();
+
+ console.log(a);
+
+ //output 10 20 100
+
+ //lexical scope
+ var x = 10;
+ function outerfun()
+ {
+   innerfun();
+   // var x = 10;
+   function innerfun()
+   {
+      console.log(x);
+      // checks value of a in local memory of execution context and then checks the lexical environment of its parent hierachicaly (c has ref to a and a has global execution context)
+   }
+ }
+outerfun();
